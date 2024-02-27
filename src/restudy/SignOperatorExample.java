1b6b82901c546a2239302aee8bdd09e135e94a29
이준우 <leejunwoo.contact@gmail.com>
2024-02-27 19:33:11 +0900

    1
    1

---
+++ src/restudy/SignOperatorExample.java
@@ -0,0 +1,18 @@
+package restudy;
+
+public class SignOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int x = -100;
+		x = -x;
+		
+		byte b = 100;
+		int y = -b;
+		
+	System.out.println("x : " + x);
+	System.out.println("y : " + y);
+	
+	
+	}
+}
